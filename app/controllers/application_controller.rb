cb14a5cc59858eafb46d0c1ba4d62c1953f51a1b
Sam Puttman <daputter2@gmail.com>
2024-04-02 20:40:10 -0400

    Add login helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  def require_login
+    redirect_to login_path, alert: 'Please log in to continue.' unless logged_in?
+  end
+
 end

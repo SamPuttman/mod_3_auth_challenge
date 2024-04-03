40e5fb9a0960eae0188e6edce53acddbfacbc109
Sam Puttman <daputter2@gmail.com>
2024-04-02 20:40:32 -0400

    Add require_login before action to movies and users pages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,13 @@
 class MoviesController < ApplicationController
-  def index 
+  before_action :require_login
+  
+  def index
       @user = User.find(params[:id])
       @movies = Movie.all
-  end 
+  end
 
-  def show 
+  def show
       @user = User.find(params[:user_id])
       @movie = Movie.find(params[:id])
-  end 
-end 
+  end
+end
--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,18 +1,20 @@
-class ViewingPartiesController < ApplicationController 
+class ViewingPartiesController < ApplicationController
+  before_action :require_login
+  
   def new
     @user = User.find(params[:user_id])
     @movie = Movie.find(params[:movie_id])
-  end 
-  
-  def create 
+  end
+
+  def create
     user = User.find(params[:user_id])
     user.viewing_parties.create(viewing_party_params)
     redirect_to "/users/#{params[:user_id]}"
-  end 
+  end
 
-  private 
+  private
 
-  def viewing_party_params 
+  def viewing_party_params
     params.permit(:movie_id, :duration, :date, :time)
-  end 
-end 
+  end
+end
